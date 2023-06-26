e6f5611af358b264241f44826483b9f8ebe32b08
Miles Georgi <azimux@gmail.com>
2023-06-26 14:43:04 -0700

    Remove frozen string literal, can add back if it really bites somebody

--- commands.gemspec
+++ commands.gemspec
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "lib/commands/version"
 
 Gem::Specification.new do |spec|
--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe Commands do
   it "has a version number" do
     expect(Commands::VERSION).not_to be_nil
