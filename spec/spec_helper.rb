93a23a2ff06acba23aacc09b52db433fab5f430a
Miles Georgi <azimux@gmail.com>
2023-07-11 12:02:13 -0700

    Allow using :focus in rspec locally but not in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require "rspec/its"
 require_relative "../load_everything"
 
 RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
