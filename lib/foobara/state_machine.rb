dcd0d0dff4e577c4005e67a05402c0871ddf58c4
Miles Georgi <azimux@gmail.com>
2023-07-14 04:03:33 +0000

    Create callback registration methods for StateMachine

--- lib/foobara/state_machine.rb
+++ lib/foobara/state_machine.rb
@@ -2,6 +2,8 @@ module Foobara
   class StateMachine
     LogEntry = Struct.new(:from_state, :transition, :to_state)
 
+    ALLOWED_CALLBACK_TYPES = %i[before after around failure error].freeze
+
     class TransitionAlreadyDefinedError < StandardError; end
     class UnexpectedTerminalStates < StandardError; end
     class MissingStates < StandardError; end
@@ -34,6 +36,7 @@ module Foobara
       create_enums
       create_state_predicate_methods
       create_transition_methods
+      create_register_callback_methods
     end
 
     def perform_transition!(transition, successful_if: nil, allowed_error_classes: [])
@@ -114,9 +117,7 @@ module Foobara
       from = from.to_sym
       to = to.to_sym
 
-      allowed_callback_types = %i[before after around]
-
-      raise "bad type #{type} expected one of #{allowed_callback_types}" unless types.include?
+      raise "bad type #{type} expected one of #{ALLOWED_CALLBACK_TYPES}" unless types.include?
 
       if transition && !transitions.include?(transition)
         raise "bad transition #{transition} expected one of #{transitions}"
@@ -141,6 +142,26 @@ module Foobara
       triple_callbacks = type_callbacks[triple] ||= []
 
       triple_callbacks << block
+    end
+
+    def before_any_transition(&)
+      register_transition_callback(:before, &)
+    end
+
+    def after_any_transition(&)
+      register_transition_callback(:after, &)
+    end
+
+    def around_any_transition(&)
+      register_transition_callback(:around, &)
+    end
+
+    def failure_any_transition(&)
+      register_transition_callback(:failure, &)
+    end
+
+    def error_any_transition(&)
+      register_transition_callback(:error, &)
     end
 
     private
@@ -420,6 +441,67 @@ module Foobara
       transitions.each do |transition|
         singleton_class.define_method "#{transition}!" do
           perform_transition!(transition)
+        end
+      end
+    end
+
+    # TODO: figure out a way to not have to declare these for every instance of the state machine, wtf
+    def create_register_callback_methods
+      # 0 before_any_transition
+      # 1 before_transition_to_<state>
+      # 2 before_transition_from_<state>
+      # 3 before_transition_from_<state>_to_<state>
+      # 4 before_<transition>_transition
+      # 5 before_<transition>_transition_to_<state>
+      # 6 before_<transition>_transition_from_<state>
+
+      tos = Set.new
+      froms = Set.new
+
+      froms_to_tos = Set.new
+      transitions_to_tos = Set.new
+      froms_to_transitions = Set.new
+
+      # ALLOWED_CALLBACK_TYPES = %i[before after around failure error]
+      transition_map.each_pair do |from, to_map|
+        froms << from
+        to_map.each_pair do |transition, to|
+          tos << to
+          froms_to_tos << [from, to]
+          transitions_to_tos << [transition, to]
+          froms_to_transitions << [from, transition]
+        end
+      end
+
+      ALLOWED_CALLBACK_TYPES.each do |type|
+        froms.each do |from|
+          singleton_class.define_method "#{type}_transition_from_#{from}" do |&block|
+            register_transition_callback(type, from:, &block)
+          end
+        end
+
+        tos.each do |to|
+          singleton_class.define_method "#{type}_transition_to_#{to}" do |&block|
+            register_transition_callback(type, to:, &block)
+          end
+        end
+
+        froms_to_tos.each do |(from, to)|
+          singleton_class.define_method "#{type}_transition_from_#{from}_to_#{to}" do |&block|
+            register_transition_callback(type, to:, from:, &block)
+          end
+        end
+
+        transitions_to_tos.each do |(transition, to)|
+          singleton_class.define_method "#{type}_#{transition}_to_#{to}" do |&block|
+            register_transition_callback(type, transition:, to:, &block)
+          end
+        end
+
+        froms_to_transitions.each do |(from, transition)|
+          singleton_class.define_method "#{type}_#{transition}_from_#{from}" do |&block|
+            register_transition_callback(type, transition:, from:, &block)
+          end
         end
       end
     end
