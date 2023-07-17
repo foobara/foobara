87a6dd09dccfb1205292055a05a556ab285d363d
Miles Georgi <azimux@gmail.com>
2023-07-17 20:19:49 +0000

    Improve how strict_schema works

--- lib/foobara/models/schema.rb
+++ lib/foobara/models/schema.rb
@@ -1,32 +1,21 @@
 module Foobara
   module Models
     class Schema
-      attr_accessor :raw_schema, :strict_schema, :errors, :schema_has_been_validated
+      attr_accessor :raw_schema, :errors, :schema_has_been_validated
 
       def initialize(raw_schema)
+        raise "must give a schema" unless raw_schema
+
         self.errors = []
         self.raw_schema = raw_schema
       end
 
       def type
-        validate_schema unless schema_validated?
-
         strict_schema[:type]
       end
 
-      def desugarize
-        self.strict_schema = case raw_schema
-                             when Symbol
-                               { type: raw_schema }
-                             when Hash
-                               if !raw_schema.key?(:type) && !raw_schema.key?(:schemas) && raw_schema.keys.all? do |key|
-                                    key.is_a?(Symbol)
-                                  end
-                                 { type: :attributes, schemas: raw_schema }
-                               end
-                             end
-
-        self.strict_schema ||= raw_schema
+      def strict_schema
+        @strict_schema ||= desugarize
       end
 
       def valid?
@@ -51,10 +40,28 @@ module Foobara
 
       private
 
+      def desugarize
+        case raw_schema
+        when Symbol
+          { type: raw_schema }
+        when Hash
+          if !raw_schema.key?(:type) && !raw_schema.key?(:schemas) && raw_schema.keys.all? do |key|
+            key.is_a?(Symbol)
+          end
+            schemas = raw_schema.transform_values do |schema|
+              Schema.new(schema).strict_schema
+            end
+
+            {
+              type: :attributes,
+              schemas:
+            }
+          end
+        end || raw_schema
+      end
+
       def validate_schema
         self.schema_has_been_validated = true
-
-        desugarize
 
         return errors if errors.present?
 
