051418fe5492d28a58c22d13ef7111472799c3b3
Miles Georgi <azimux@gmail.com>
2023-07-18 16:15:17 +0000

    Move Outcome into Command namespace

--- src/foobara/command/outcome.rb
+++ src/foobara/command/outcome.rb
@@ -1,44 +1,46 @@
 module Foobara
-  class Outcome
-    class << self
-      def success(result)
-        new.tap do |outcome|
-          outcome.result = result
-        end
-      end
-
-      def errors(error_collection)
-        if error_collection.is_a?(Array)
+  class Command
+    class Outcome
+      class << self
+        def success(result)
           new.tap do |outcome|
-            error_collection.each do |error|
-              outcome.add_error(error)
-            end
+            outcome.result = result
           end
-        else
-          new(error_collection:)
+        end
+
+        def errors(error_collection)
+          if error_collection.is_a?(Array)
+            new.tap do |outcome|
+              error_collection.each do |error|
+                outcome.add_error(error)
+              end
+            end
+          else
+            new(error_collection:)
+          end
+        end
+
+        def error(error)
+          errors(Array.wrap(error))
         end
       end
 
-      def error(error)
-        errors(Array.wrap(error))
+      attr_writer :result
+      attr_reader :error_collection
+
+      def initialize(error_collection: ErrorCollection.new)
+        @error_collection = error_collection
       end
-    end
 
-    attr_writer :result
-    attr_reader :error_collection
+      delegate :has_errors?, :errors, :each_error, :has_error?, :add_error, to: :error_collection
 
-    def initialize(error_collection: ErrorCollection.new)
-      @error_collection = error_collection
-    end
+      def success?
+        !has_errors?
+      end
 
-    delegate :has_errors?, :errors, :each_error, :has_error?, :add_error, to: :error_collection
-
-    def success?
-      !has_errors?
-    end
-
-    def result
-      success? ? @result : nil
+      def result
+        success? ? @result : nil
+      end
     end
   end
 end
