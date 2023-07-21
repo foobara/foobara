d6bdf555c8bc5e1209d2396c93abaa4ebfa9fc35
Miles Georgi <azimux@gmail.com>
2023-07-21 20:58:56 +0000

    Load things in commands.rb in a way more like real gems

--- src/commands.rb
+++ src/commands.rb
@@ -4,13 +4,34 @@ require "active_support/core_ext/hash/indifferent_access"
 require "active_support/core_ext/module/delegation"
 require "active_support/core_ext/string/inflections"
 
+# TODO: do we really want this module/project name?
 module Commands
 end
 
-lib_files = Dir["#{__dir__}/../lib/**/*.rb"].sort_by(&:length).reverse
-src_files = Dir["#{__dir__}/**/*.rb"].sort_by(&:length).reverse
+# TODO: break these out into separate gems instead of simulating it here
 
-type, lib_files = lib_files.partition { |f| f.end_with?("lib/foobara/model/type.rb") }
-lib_files, domain_files = lib_files.partition { |f| f !~ %r{\.\./lib/foobara/domain(/|\.rb)} }
+require "foobara"
 
-[type, lib_files, src_files, domain_files].flatten.each { |f| require f }
+require "foobara/util"
+
+require "foobara/error"
+require "foobara/error_collection"
+require "foobara/multiple_error"
+
+require "foobara/outcome"
+
+Foobara::Util.require_directory("#{__dir__}/../lib/foobara/enumerated")
+
+require "foobara/callback"
+Foobara::Util.require_directory("#{__dir__}/../lib/foobara/callback")
+
+Foobara::Util.require_directory("#{__dir__}/../lib/foobara/state_machine")
+require "foobara/state_machine"
+
+Foobara::Util.require_directory("#{__dir__}/../lib/foobara/model")
+require "foobara/model"
+
+Foobara::Util.require_directory("#{__dir__}/../src/")
+
+require "foobara/domain"
+Foobara::Util.require_directory("#{__dir__}/../lib/foobara/domain")
