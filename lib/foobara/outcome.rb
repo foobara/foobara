8c1149dd9b2d4c7e8509e8aaebec2c0f0ce52498
Miles Georgi <azimux@gmail.com>
2023-07-21 14:37:39 +0000

    Move Outcome into Foobara

--- lib/foobara/outcome.rb
+++ lib/foobara/outcome.rb
@@ -1,46 +1,44 @@
 module Foobara
-  class Command
-    class Outcome
-      class << self
-        def success(result)
+  class Outcome
+    class << self
+      def success(result)
+        new.tap do |outcome|
+          outcome.result = result
+        end
+      end
+
+      def errors(error_collection)
+        if error_collection.is_a?(Array)
           new.tap do |outcome|
-            outcome.result = result
-          end
-        end
-
-        def errors(error_collection)
-          if error_collection.is_a?(Array)
-            new.tap do |outcome|
-              error_collection.each do |error|
-                outcome.add_error(error)
-              end
+            error_collection.each do |error|
+              outcome.add_error(error)
             end
-          else
-            new(error_collection:)
           end
-        end
-
-        def error(error)
-          errors(Array.wrap(error))
+        else
+          new(error_collection:)
         end
       end
 
-      attr_writer :result
-      attr_reader :error_collection
-
-      def initialize(error_collection: ErrorCollection.new)
-        @error_collection = error_collection
+      def error(error)
+        errors(Array.wrap(error))
       end
+    end
 
-      delegate :has_errors?, :errors, :each_error, :has_error?, :add_error, to: :error_collection
+    attr_writer :result
+    attr_reader :error_collection
 
-      def success?
-        !has_errors?
-      end
+    def initialize(error_collection: ErrorCollection.new)
+      @error_collection = error_collection
+    end
 
-      def result
-        success? ? @result : nil
-      end
+    delegate :has_errors?, :errors, :each_error, :has_error?, :add_error, to: :error_collection
+
+    def success?
+      !has_errors?
+    end
+
+    def result
+      success? ? @result : nil
     end
   end
 end
