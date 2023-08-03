5fc13ef527e4e35ae227e0975821398033941ec3
Miles Georgi <azimux@gmail.com>
2023-08-03 23:09:26 +0000

    Move context_schema directly onto AttributeError

--- lib/foobara/type/attribute_error.rb
+++ lib/foobara/type/attribute_error.rb
@@ -5,6 +5,16 @@ module Foobara
     class AttributeError < Type::TypeError
       attr_accessor :path
 
+      class << self
+        def context_schema
+          {
+            path: :duck, # TODO: fix this up once there's an array type
+            attribute_name: :symbol,
+            value: :duck
+          }
+        end
+      end
+
       def initialize(context:, path: [], **data)
         super(**data)
 
@@ -12,6 +22,8 @@ module Foobara
 
         self.path = path
       end
+
+      delegate :context_schema, to: :class
 
       def attribute_name
         # TODO: feels awkward... something is not right
