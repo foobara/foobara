1c2891d3dd17bfa2f61c5ef3c5e44c5ded88cf11
Miles Georgi <azimux@gmail.com>
2023-08-04 19:14:07 -0700

    Fix bug polluting Module with transition callback methods

--- src/foobara/command/concerns/callbacks.rb
+++ src/foobara/command/concerns/callbacks.rb
@@ -28,47 +28,50 @@ module Foobara
           delegate :remove_all_callbacks, to: :callback_state_machine_target
         end
 
-        [self, self.class].each do |target|
-          %i[before after].each do |type|
-            target.define_method "#{type}_any_transition" do |&block|
-              callback_state_machine_target.register_transition_callback(type) do |state_machine:, **args|
-                block.call(command: state_machine.owner, **args)
-              end
-            end
-          end
-
-          target.define_method "around_any_transition" do |&block|
-            callback_state_machine_target.register_transition_callback(
-              :around
-            ) do |state_machine:, **args, &do_transition_block|
-              block.call(command: state_machine.owner, **args, &do_transition_block)
-            end
-          end
-        end
-
-        Foobara::Command::StateMachine.transitions.each do |transition|
-          [self, self.class].each do |target|
+        included do
+          # TODO: this seems to be polluting Object wtf...
+          [self, singleton_class].each do |target|
             %i[before after].each do |type|
-              target.define_method "#{type}_#{transition}" do |&block|
-                callback_state_machine_target.register_transition_callback(type,
-                                                                           transition:) do |state_machine:, **args|
+              target.define_method "#{type}_any_transition" do |&block|
+                callback_state_machine_target.register_transition_callback(type) do |state_machine:, **args|
                   block.call(command: state_machine.owner, **args)
                 end
               end
             end
 
-            target.define_method "around_#{transition}" do |&block|
+            target.define_method "around_any_transition" do |&block|
               callback_state_machine_target.register_transition_callback(
                 :around
               ) do |state_machine:, **args, &do_transition_block|
                 block.call(command: state_machine.owner, **args, &do_transition_block)
               end
             end
+          end
 
-            %i[error].each do |type|
-              target.define_method "#{type}_any_transition" do |&block|
-                callback_state_machine_target.register_transition_callback(type) do |error:, state_machine:, **args|
-                  block.call(error:, command: state_machine.owner, **args)
+          Foobara::Command::StateMachine.transitions.each do |transition|
+            [self, singleton_class].each do |target|
+              %i[before after].each do |type|
+                target.define_method "#{type}_#{transition}" do |&block|
+                  callback_state_machine_target.register_transition_callback(type,
+                                                                             transition:) do |state_machine:, **args|
+                    block.call(command: state_machine.owner, **args)
+                  end
+                end
+              end
+
+              target.define_method "around_#{transition}" do |&block|
+                callback_state_machine_target.register_transition_callback(
+                  :around
+                ) do |state_machine:, **args, &do_transition_block|
+                  block.call(command: state_machine.owner, **args, &do_transition_block)
+                end
+              end
+
+              %i[error].each do |type|
+                target.define_method "#{type}_any_transition" do |&block|
+                  callback_state_machine_target.register_transition_callback(type) do |error:, state_machine:, **args|
+                    block.call(error:, command: state_machine.owner, **args)
+                  end
                 end
               end
             end
