44b1497d048e3fe9a6db3215264fbbf2a7b4860f
Miles Georgi <azimux@gmail.com>
2023-08-11 12:20:57 -0700

    Empty TypeDeclaration class

---
+++ lib/foobara/types/type_declaration.rb
@@ -0,0 +1,16 @@
+module Foobara
+  module Types
+    # This will replace Schema...
+    # This is like Type
+    # Instead of casters/transformers we have can_handle? and desugarizers
+    # instead of validators we have declaration validators
+    # process:
+    #   Make sure we can handle this
+    #   desugarize
+    #   validate declaration value
+    #   transform into Type instance
+    # So... sugary type declaration value in, type out
+    class TypeDeclaration < Type
+    end
+  end
+end
