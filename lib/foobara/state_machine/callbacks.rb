d0b994928af94cad4b035370ef4a323325a3f7f6
Miles Georgi <azimux@gmail.com>
2023-08-10 21:46:50 -0700

    Get state_machine/callbacks.rb to 100% coverage

--- lib/foobara/state_machine/callbacks.rb
+++ lib/foobara/state_machine/callbacks.rb
@@ -6,7 +6,7 @@ module Foobara
       # owner helps with determining the relevant object when running class-registered state transition callbacks
       attr_accessor :callback_registry, :owner
 
-      def initialize(owner:)
+      def initialize(owner: nil)
         self.owner = owner
         self.callback_registry = Callback::Registry::ChainedConditioned.new(self.class.class_callback_registry)
       end
@@ -51,24 +51,32 @@ module Foobara
         # 5 before_<transition>_transition_to_<state>
         # 6 before_<transition>_transition_from_<state>
         def create_register_callback_methods
-          raise "Do not create register callback methods twice" if register_callback_methods
+          if register_callback_methods
+            # :nocov:
+            raise "Do not create register callback methods twice"
+            # :nocov:
+          end
 
           callback_methods = @register_callback_methods = []
 
           tos = Set.new
+          transitions = Set.new
           froms = Set.new
 
           froms_to_tos = Set.new
           transitions_to_tos = Set.new
           froms_to_transitions = Set.new
+          froms_transitions_tos = Set.new
 
           transition_map.each_pair do |from, to_map|
             froms << from
             to_map.each_pair do |transition, to|
+              transitions << transition
               tos << to
               froms_to_tos << [from, to]
               transitions_to_tos << [transition, to]
               froms_to_transitions << [from, transition]
+              froms_transitions_tos << [from, transition, to]
             end
           end
 
@@ -86,6 +94,15 @@ module Foobara
 
               define_method method_name do |&block|
                 register_transition_callback(type, from:, &block)
+              end
+            end
+
+            transitions.each do |transition|
+              method_name = "#{type}_#{transition}"
+              callback_methods << method_name
+
+              define_method method_name do |&block|
+                register_transition_callback(type, transition:, &block)
               end
             end
 
@@ -122,6 +139,15 @@ module Foobara
 
               define_method method_name do |&block|
                 register_transition_callback(type, transition:, from:, &block)
+              end
+            end
+
+            froms_transitions_tos.each do |(from, transition, to)|
+              method_name = "#{type}_#{transition}_from_#{from}_to_#{to}"
+              callback_methods << method_name
+
+              define_method method_name do |&block|
+                register_transition_callback(type, transition:, from:, to:, &block)
               end
             end
           end
---
+++ spec/lib/state_machine/callbacks_spec.rb
@@ -0,0 +1,152 @@
+RSpec.describe Foobara::StateMachine::Callbacks do
+  subject { state_machine_class }
+
+  let(:state_machine_class) do
+    Class.new(Foobara::StateMachine).tap do |klass|
+      klass.set_transition_map(
+        transition_map
+      )
+    end
+  end
+
+  let(:state_machine) {
+    state_machine_class.new
+  }
+
+  let(:transition_map) do
+    {
+      unexecuted: {
+        start: :running
+      },
+      running: {
+        fail: :failed,
+        succeed: :succeeded,
+        reset: :unexecuted
+      }
+    }
+  end
+
+  describe "dynamic transition callback helpers" do
+    # before after around error any
+    # from
+    # to
+    # from to
+    # tx from
+    # tx to
+    let(:calls) { [] }
+    let(:from) { :unexecuted }
+    let(:to) { :running }
+    let(:transition) { :start }
+
+    def set_callback(callback_name)
+      record_call = ->(**_opts) { calls << callback_name }
+
+      block = case callback_name
+              when /^around_/
+                ->(**_opts, &do_it) {
+                  record_call.call
+                  do_it.call
+                }
+              when /^error_/
+                ->(_error) { record_call.call }
+              else
+                record_call
+              end
+
+      state_machine.send(callback_name, &block)
+    end
+
+    it "calls them all" do
+      %i[before around after error].each do |type|
+        [
+          "#{type}_any_transition",
+          "#{type}_transition_from_#{from}",
+          "#{type}_transition_to_#{to}",
+          "#{type}_transition_from_#{from}_to_#{to}",
+          "#{type}_#{transition}",
+          "#{type}_#{transition}_from_#{from}",
+          "#{type}_#{transition}_to_#{to}",
+          "#{type}_#{transition}_from_#{from}_to_#{to}"
+        ].each do |callback_name|
+          set_callback(callback_name)
+        end
+      end
+
+      state_machine.perform_transition!(transition)
+
+      # 8 for each type but error type won't be called here
+      expect(calls.size).to eq(24)
+
+      expect(calls).to match_array(
+        %w[before_start_from_unexecuted_to_running
+           before_transition_from_unexecuted_to_running
+           before_start_from_unexecuted
+           before_transition_from_unexecuted
+           before_start_to_running
+           before_transition_to_running
+           before_start
+           before_any_transition
+           around_any_transition
+           around_start
+           around_transition_to_running
+           around_start_to_running
+           around_transition_from_unexecuted
+           around_start_from_unexecuted
+           around_transition_from_unexecuted_to_running
+           around_start_from_unexecuted_to_running
+           after_start_from_unexecuted_to_running
+           after_transition_from_unexecuted_to_running
+           after_start_from_unexecuted
+           after_transition_from_unexecuted
+           after_start_to_running
+           after_transition_to_running
+           after_start
+           after_any_transition]
+      )
+
+      state_machine.reset!
+
+      calls.clear
+
+      state_machine.around_any_transition do |&do_it|
+        do_it.call
+        raise "kaboom"
+      end
+
+      expect {
+        state_machine.start!
+      }.to raise_error("kaboom")
+
+      # 8 before, 8 around, and 8 error callbacks (we never make it to after because of the raise)
+      expect(calls.size).to eq(24)
+      expect(calls).to match_array(
+        %w[
+          before_start_from_unexecuted_to_running
+          before_transition_from_unexecuted_to_running
+          before_start_from_unexecuted
+          before_transition_from_unexecuted
+          before_start_to_running
+          before_transition_to_running
+          before_start
+          before_any_transition
+          around_any_transition
+          around_start
+          around_transition_to_running
+          around_start_to_running
+          around_transition_from_unexecuted
+          around_start_from_unexecuted
+          around_transition_from_unexecuted_to_running
+          around_start_from_unexecuted_to_running
+          error_start_from_unexecuted_to_running
+          error_transition_from_unexecuted_to_running
+          error_start_from_unexecuted
+          error_transition_from_unexecuted
+          error_start_to_running
+          error_transition_to_running
+          error_start
+          error_any_transition
+        ]
+      )
+    end
+  end
+end
