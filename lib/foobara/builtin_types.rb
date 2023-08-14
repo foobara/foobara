a711448b3acf12fe97a7e57205b65a2ee395cb71
Miles Georgi <azimux@gmail.com>
2023-08-14 13:12:34 -0700

    Eliminate types/builtin_types.rb

--- lib/foobara/builtin_types.rb
+++ lib/foobara/builtin_types.rb
@@ -1,5 +1,38 @@
 module Foobara
-  class Model
+  module BuiltinTypes
+    class << self
+      # TODO: do we want a TypeRegistry class of some sort?
+      def [](symbol)
+        unless registered?(symbol)
+          # :nocov:
+          raise "No type registered for symbol #{symbol.inspect}"
+          # :nocov:
+        end
+
+        registry[symbol]
+      end
+
+      def []=(symbol, type)
+        if registry.key?(symbol)
+          # :nocov:
+          raise "#{symbol} was already registered"
+          # :nocov:
+        end
+
+        registry[symbol] = type
+      end
+
+      def registered?(symbol)
+        registry.key?(symbol)
+      end
+
+      private
+
+      def registry
+        @registry ||= {}
+      end
+    end
+
     [
       Schemas::Duck,
       Schemas::Symbol,
