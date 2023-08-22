1527fce000668994ffac5b98d726b9c2c600bc6f
Miles Georgi <azimux@gmail.com>
2023-08-22 10:07:29 -0700

    Fix bug preventing declaration validators from being applied

--- lib/foobara/type_declarations/type_declaration_validator.rb
+++ lib/foobara/type_declarations/type_declaration_validator.rb
@@ -2,6 +2,10 @@ module Foobara
   module TypeDeclarations
     class TypeDeclarationValidator < Value::Validator
       include WithRegistries
+
+      def always_applicable?
+        true
+      end
     end
   end
 end
