65be6e60cd116c8e6fe3fc8bcd96459d135a6dee
Miles Georgi <azimux@gmail.com>
2023-08-28 21:30:17 -0700

    Allow access to WithRegistries methods from class-level

--- lib/foobara/type_declarations/with_registries.rb
+++ lib/foobara/type_declarations/with_registries.rb
@@ -1,21 +1,31 @@
 module Foobara
   module TypeDeclarations
     module WithRegistries
-      def type_for_declaration(...)
-        Namespace.current.type_for_declaration(...)
+      extend ActiveSupport::Concern
+
+      class_methods do
+        def type_for_declaration(...)
+          Namespace.current.type_for_declaration(...)
+        end
+
+        def type_declaration_handler_for(...)
+          Namespace.current.type_declaration_handler_for(...)
+        end
+
+        def type_for_symbol(...)
+          Namespace.current.type_for_symbol(...)
+        end
+
+        def type_registered?(...)
+          Namespace.current.type_registered?(...)
+        end
       end
 
-      def type_declaration_handler_for(...)
-        Namespace.current.type_declaration_handler_for(...)
-      end
-
-      def type_for_symbol(...)
-        Namespace.current.type_for_symbol(...)
-      end
-
-      def type_registered?(...)
-        Namespace.current.type_registered?(...)
-      end
+      delegate :type_for_declaration,
+               :type_declaration_handler_for,
+               :type_for_symbol,
+               :type_registered?,
+               to: :class
     end
   end
 end
