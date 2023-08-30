9b89b56993e7ff64bb15a0fc52f7e8141538a2a4
Miles Georgi <azimux@gmail.com>
2023-08-30 17:40:09 +0000

    Get ErrorCollection to 100%

--- spec/lib/foobara/common/error_collection_spec.rb
+++ spec/lib/foobara/common/error_collection_spec.rb
@@ -15,18 +15,51 @@ RSpec.describe Foobara::ErrorCollection do
     let(:error_class) { Foobara::Error.subclass(symbol:, message:, context_type_declaration: { foo: :symbol }) }
     let(:error) { error_class.new(context:) }
 
-    it "creates and adds an error" do
-      expect(error_collection).to be_empty
+    context "when adding error instance" do
+      it "creates and adds an error" do
+        expect(error_collection).to be_empty
 
-      error_collection.add_error(error)
+        error_collection.add_error(error)
 
-      expect(error_collection).to_not be_empty
-      expect(error_collection.size).to eq(1)
-      error = error_collection.errors.first
-      expect(error).to be_a(Foobara::Error)
-      expect(error.symbol).to eq(symbol)
-      expect(error.message).to eq(message)
-      expect(error.context).to eq(context)
+        expect(error_collection).to_not be_empty
+        expect(error_collection.size).to eq(1)
+        error = error_collection.errors.first
+        expect(error).to be_a(Foobara::Error)
+        expect(error.symbol).to eq(symbol)
+        expect(error.message).to eq(message)
+        expect(error.context).to eq(context)
+      end
+    end
+
+    # Do we really need to support this?
+    context "when adding error by hash" do
+      around do |example|
+        Foobara::TypeDeclarations.with_validate_error_context_disabled do
+          example.run
+        end
+      end
+
+      let(:error_hash) do
+        {
+          symbol:,
+          message:,
+          context:
+        }
+      end
+
+      it "creates and adds an error" do
+        expect(error_collection).to be_empty
+
+        error_collection.add_error(error_hash)
+
+        expect(error_collection).to_not be_empty
+        expect(error_collection.size).to eq(1)
+        error = error_collection.errors.first
+        expect(error).to be_a(Foobara::Error)
+        expect(error.symbol).to eq(symbol)
+        expect(error.message).to eq(message)
+        expect(error.context).to eq(context)
+      end
     end
 
     context "when passing the same error twice" do
