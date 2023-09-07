b4e4bc39b4c860ff076ff839cd260ce8ff465789
Miles Georgi <azimux@gmail.com>
2023-09-07 03:08:02 +0000

    Enforce 100% line coverage in the test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,8 @@ require "simplecov"
 
 SimpleCov.start do
   enable_coverage :branch
-  # TODO: enable this
+  minimum_coverage line: 100
+  # TODO: enable this? worth it to get to 100% branch coverage?
   # minimum_coverage line: 100, branch: 100
 end
 
