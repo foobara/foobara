eba8dac5abd54e138ebba52d5846350cbaebd3d6
Miles Georgi <azimux@gmail.com>
2023-09-21 00:58:44 +0000

    thread parent

--- lib/foobara.rb
+++ lib/foobara.rb
@@ -4,6 +4,7 @@ require "active_support/core_ext/hash/indifferent_access"
 require "active_support/core_ext/module/delegation"
 require "active_support/core_ext/string/inflections"
 
+require "foobara/thread_parent"
 # TODO: break these out into separate gems instead of simulating it here
 require "foobara/common"
 require "foobara/value"
