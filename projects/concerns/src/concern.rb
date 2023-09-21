8c03774aded779d001f3eb7ed9be027c34cfb3bc
Miles Georgi <azimux@gmail.com>
2023-09-21 23:39:40 +0000

    eliminate another

--- projects/concerns/src/concern.rb
+++ projects/concerns/src/concern.rb
@@ -1,12 +1,26 @@
 module Foobara
   module Concern
+    module InstallClassMethods
+      def included(klass)
+        if const_defined?(:ClassMethods)
+          klass.extend(const_get(:ClassMethods))
+        end
+
+        if @foobara_on_include
+          klass.class_eval(&@foobara_on_include)
+        end
+
+        super
+      end
+
+      def on_include(&block)
+        @foobara_on_include = block
+      end
+    end
+
     class << self
       def included(concern)
-        concern.singleton_class.define_method :included do |klass|
-          if concern.const_defined?(:ClassMethods)
-            klass.extend(concern.const_get(:ClassMethods))
-          end
-        end
+        concern.extend(InstallClassMethods)
       end
     end
   end
