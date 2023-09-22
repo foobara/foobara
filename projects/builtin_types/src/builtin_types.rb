d30246cd7a1a1bb1204c3ff7b83ec79f0a961a10
Miles Georgi <azimux@gmail.com>
2023-09-22 22:25:59 +0000

    move some builtin_types installation code back to lib/

--- projects/builtin_types/src/builtin_types.rb
+++ projects/builtin_types/src/builtin_types.rb
@@ -13,48 +13,6 @@ module Foobara
         build_and_register_all_builtins_and_install_type_declaration_extensions!
       end
 
-      def build_and_register_all_builtins_and_install_type_declaration_extensions!
-        duck = build_and_register!(:duck, nil, ::Object)
-        # TODO: should we ban ::Object that are ::Enumerable from atomic_duck?
-        atomic_duck = build_and_register!(:atomic_duck, duck, ::Object)
-        build_and_register!(:symbol, atomic_duck)
-        # TODO: wtf why pass ::Object? It's to avoid casting? Do we need a way to flag abstract types?
-        number = build_and_register!(:number, atomic_duck, ::Object)
-        build_and_register!(:integer, number)
-        build_and_register!(:float, number)
-        build_and_register!(:big_decimal, number)
-        # Let's skip these for now since they rarely come up in business contexts and both could be
-        # represented by a tuple of numbers.
-        # build_and_register!(:rational, number)
-        # build_and_register!(:complex, number)
-        string = build_and_register!(:string, atomic_duck)
-        build_and_register!(:date, atomic_duck)
-        build_and_register!(:datetime, atomic_duck, ::Time)
-        build_and_register!(:boolean, atomic_duck, [::TrueClass, ::FalseClass])
-        build_and_register!(:email, string, ::String)
-        # TODO: not urgent and derisked already via :email
-        # phone_number = build_and_register!(:phone_number, string)
-        # TODO: wtf
-        duckture = build_and_register!(:duckture, duck, ::Object)
-        array = build_and_register!(:array, duckture)
-        build_and_register!(:tuple, array, ::Array)
-        associative_array = build_and_register!(:associative_array, duckture, ::Hash)
-        # TODO: uh oh... we do some translations in the casting here...
-        build_and_register!(:attributes, associative_array, nil)
-        # What does a model have that :attributes doesnt have?
-        #   name
-        #   a target class (can default to a dynamically created Foobara::Model ??)
-        #     #valid?
-        #     #attributes
-        #     readers/writers for all attributes
-        # NOTE: A model isn't attributes!!! it has attributes. It is instead an atomic duck.
-        # Subtypes should not use ::Hash as the target type. This is a confusing hack.
-        model = build_and_register!(:model, atomic_duck, nil)
-        build_and_register!(:entity, model, nil)
-        # address = build_and_register!(:address, model)
-        # us_address = build_and_register!(:us_address, model)
-      end
-
       def build_and_register!(type_symbol, base_type, target_classes = const_get("::#{Util.classify(type_symbol)}"))
         type = build_from_modules_and_install_type_declaration_extensions!(type_symbol, target_classes, base_type)
 
@@ -175,7 +133,5 @@ module Foobara
         end
       end
     end
-
-    build_and_register_all_builtins_and_install_type_declaration_extensions!
   end
 end
