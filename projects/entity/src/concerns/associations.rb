2332b03366a85a41a652238ddcb1b63ed62bba40
Miles Georgi <azimux@gmail.com>
2023-09-26 22:10:32 +0000

    Add #that_owns/#association methods and various other association methods

--- projects/entity/src/concerns/associations.rb
+++ projects/entity/src/concerns/associations.rb
@@ -9,6 +9,106 @@ module Foobara
             @associations ||= construct_associations
           end
 
+          def deep_associations
+            @deep_associations ||= begin
+              deep = {}
+
+              associations.each_pair do |data_path, type|
+                deep[data_path] = type
+
+                entity_class = type.target_classes.first
+
+                entity_class.deep_associations.each_pair do |sub_data_path, sub_type|
+                  deep["#{data_path}.#{sub_data_path}"] = sub_type
+                end
+              end
+
+              deep
+            end
+          end
+
+          # TODO: stamp this metadata out somewhere, preferably on deep_associations hash somehow
+          def association(name, *association_identifiers)
+            target_association_key = association_for(association_identifiers)
+
+            is_many = target_association_key.include?("#")
+
+            define_method name do
+              # TODO: memoize but with some smart cache busting
+              values = Foobara::DataPath.values_at(target_association_key, self)
+
+              if is_many
+                values
+              else
+                if values.size > 1
+                  # :nocov:
+                  raise "Multiple records found for #{name} association but only expected 0 or 1."
+                  # :nocov:
+                end
+
+                unless values.empty?
+                  values.first
+                end
+              end
+            end
+          end
+
+          def association_for(association_filters)
+            if association_filters.size == 1
+              data_path = association_filters.first.to_s
+
+              if deep_associations.key?(data_path)
+                return data_path
+              end
+            end
+
+            result = association_filters.inject(deep_associations.keys) do |filtered, filter|
+              filtered_associations(filter, filtered)
+            end
+
+            if result.empty?
+              # :nocov:
+              raise "Could not find association matching #{association_filters}"
+              # :nocov:
+            elsif result.size > 1
+              # :nocov:
+              raise "Multiple associations matched by #{association_filters}"
+              # :nocov:
+            else
+              result.first
+            end
+          end
+
+          def filtered_associations(filter, association_keys = deep_associations.keys)
+            if filter.is_a?(::Symbol)
+              filter = filter.to_s
+            end
+
+            if filter.is_a?(::String)
+              if filter =~ /[A-Z]/
+                association_keys.select do |key|
+                  type = deep_associations[key]
+                  entity_class = type.target_classes.first
+                  entity_class.full_entity_name.include?(filter)
+                end
+              else
+                association_keys.select do |key|
+                  key.include?(filter)
+                end
+              end
+            elsif filter.is_a?(::Class) && filter < Entity
+              association_keys.select do |key|
+                type = deep_associations[key]
+                entity_class = type.target_classes.first
+                entity_class == filter || entity_class < filter
+              end
+            else
+              # :nocov:
+              raise "Not sure how to apply filter #{filter}"
+              # :nocov:
+            end
+          end
+
           def construct_associations(type = attributes_type, path = DataPath.new, result = {})
             if type.extends_type?(namespace.type_for_symbol(:entity))
               result[path.to_s] = type
@@ -53,6 +153,55 @@ module Foobara
               # :nocov:
               contains_associations?(type.key_type, false) || contains_associations?(type.value_type, false)
               # :nocov:
+            end
+          end
+
+          def that_owns(record, filters = [])
+            association_key = association_for([record.class, *filters])
+
+            data_path = DataPath.new(association_key)
+
+            done = false
+
+            containing_records = Util.array(record)
+
+            until done
+              last = data_path.path.last
+
+              if last == :"#"
+                method = :find_all_by_attribute_containing_any_of
+                attribute_name = data_path.path[-2]
+                data_path = DataPath.new(data_path.path[0..-3])
+              else
+                method = :find_all_by_attribute_any_of
+                attribute_name = last
+                data_path = DataPath.new(data_path.path[0..-2])
+              end
+
+              containing_entity_class_path = data_path.to_s
+
+              entity_class = if containing_entity_class_path.empty?
+                               done = true
+                               self
+                             else
+                               deep_associations[
+                                 containing_entity_class_path
+                               ].target_classes.first
+                             end
+
+              containing_records = entity_class.send(method, attribute_name, containing_records).to_a
+
+              done = true unless containing_records
+            end
+
+            unless containing_records.empty?
+              if containing_records.size == 1
+                containing_records.first
+              else
+                # :nocov:
+                raise "Expected only one record to own #{record} but found #{containing_records.size}"
+                # :nocov:
+              end
             end
           end
         end
---
+++ spec/foobara/entity/that_owns_spec.rb
@@ -0,0 +1,203 @@
+RSpec.describe Foobara::Entity do
+  # Entity types:
+  #
+  # one-to-one (default for foo: Bar attribute)
+  # one-to-many (default for foo: [Bar] attribute)
+  # many-to-one
+  # many-to-many
+  #
+  # .that_own is for many-to-*
+  # .that_owns is for one-to-*
+  #
+  # To specify many-to-* one must add:
+  #
+  # associations: {
+  #   foo: {
+  #     cardinality: "many-to-one"
+  #   }
+  # }
+  #
+  # otherwise an exception will be thrown from .that_own
+  describe ".that_owns" do
+    after do
+      Foobara.reset_alls
+    end
+
+    before do
+      Foobara::Persistence.default_crud_driver = Foobara::Persistence::CrudDrivers::InMemory.new
+
+      stub_class = ->(klass) { stub_const(klass.name, klass) }
+
+      Class.new(Foobara::Entity) do
+        class << self
+          def name
+            "User"
+          end
+        end
+
+        stub_class.call(self)
+
+        attributes id: :integer,
+                   name: :string
+
+        primary_key :id
+      end
+
+      Class.new(Foobara::Entity) do
+        class << self
+          def name
+            "Applicant"
+          end
+        end
+
+        stub_class.call(self)
+
+        attributes id: :integer,
+                   user: User
+
+        primary_key :id
+      end
+
+      Class.new(Foobara::Entity) do
+        class << self
+          def name
+            "Package"
+          end
+        end
+
+        stub_class.call(self)
+
+        attributes id: :integer,
+                   applicants: [Applicant],
+                   is_active: :boolean
+
+        primary_key :id
+      end
+
+      Class.new(Foobara::Entity) do
+        class << self
+          def name
+            "Assignment"
+          end
+        end
+
+        stub_class.call(self)
+
+        attributes id: :integer,
+                   package: Package
+
+        primary_key :id
+      end
+
+      Class.new(Foobara::Entity) do
+        class << self
+          def name
+            "Employee"
+          end
+        end
+
+        stub_class.call(self)
+
+        attributes id: :integer,
+                   user: User,
+                   assignments: { type: :array, element_type_declaration: Assignment, default: [] },
+                   past_assignments: [Assignment],
+                   priority_assignment: Assignment
+
+        primary_key :id
+
+        association :past_users, "past_assignments.#.package.applicants.#.user"
+        association :priority_package, :"priority_assignment.package"
+      end
+    end
+
+    it "can find the appropriate records through various that_owns/that_own calls" do
+      expect(Employee.filtered_associations(:Assignment)).to eq(
+        ["assignments.#", "past_assignments.#", "priority_assignment"]
+      )
+
+      User.transaction do
+        applicants = []
+        employees = []
+        packages  = []
+        users = []
+
+        10.times do |i|
+          user = User.create(name: "applicant user#{i}")
+          users << user
+          applicants << Applicant.create(user:)
+        end
+
+        10.times do |i|
+          user = User.create(name: "employee user#{i}")
+          employees << Employee.create(user:)
+        end
+
+        20.times do |i|
+          packages << Package.create(is_active: i < 3, applicants: [applicants[i % 3]])
+        end
+
+        15.times do |i|
+          package = packages[i]
+
+          assignment = Assignment.create(package:)
+
+          if i < 10
+            employees[i % 4].assignments += [assignment]
+
+            if i.even?
+              employees[(i % 4) + 1].assignments += [assignment]
+            end
+          else
+            employees[0].past_assignments ||= []
+            employees[0].past_assignments += [assignment]
+          end
+        end
+
+        employee = Employee.all[0]
+        employee.priority_assignment = employee.assignments.first
+
+        expect(Employee.all[1].past_users).to eq([])
+        expect(Employee.all[0].past_users).to contain_exactly(users[0], users[1], users[2])
+
+        applicant = Applicant.all.first
+        user = applicant.user
+
+        expect(Applicant.that_owns(user)).to be(applicant)
+        expect(Employee.that_owns(users[0], "past_")).to eq(Employee.all.first)
+      end
+
+      employee_id = nil
+      assignment_id = nil
+
+      User.transaction do
+        expect(Employee.all[1].past_users).to eq([])
+        # TODO: create .first query
+        employee = Employee.all.first
+        employee_id = employee.primary_key
+
+        expect(employee.past_users).to contain_exactly(User.thunk(1), User.thunk(2), User.thunk(3))
+        expect(employee.past_users).to eq(employee.values_at("past_assignments.#.package.applicants.#.user"))
+        assignment_id = employee.past_assignments.first
+        expect(
+          Employee.find_by_attribute_containing(:past_assignments, employee.past_assignments.first)
+        ).to be(employee)
+
+        applicant = Applicant.all.first
+        user = applicant.user
+
+        expect(Applicant.that_owns(user)).to be(applicant)
+        expect(Employee.that_owns(User.thunk(1), "past_")).to eq(Employee.all.first)
+
+        expect(employee.priority_package).to be_a(Package)
+      end
+
+      User.transaction do
+        expect(
+          Employee.find_by_attribute_containing(:past_assignments, assignment_id).primary_key
+        ).to be(employee_id)
+        expect(User.find_by_attribute(:name, "applicant user5")).to be(User.thunk(6))
+      end
+    end
+  end
+end
