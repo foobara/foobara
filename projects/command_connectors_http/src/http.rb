b6d44744e4711de1ea8ab7500b1605ba7d60d496
Miles Georgi <azimux@gmail.com>
2023-10-03 23:02:41 +0000

    99.68

--- projects/command_connectors_http/src/http.rb
+++ projects/command_connectors_http/src/http.rb
@@ -1,43 +1,23 @@
 module Foobara
   module CommandConnectors
     class Http < CommandConnector
-      def not_allowed_to_run_reasons(registry_entry, command)
-        # TODO: Need to move the command into the load_records state but not close the transaction...
-        allowed_rule = registry_entry.allowed_rule
-
-        return nil unless allowed_rule
-
-        unless command.instance_eval(&allowed_rule.block)
-          allowed_rule.explanation
-        end
-      end
-
       def context_to_request(**context)
         path = context[:path]
 
         action, full_command_name = path[1..].split("/")
 
         if action != "run"
-          command_name = case action
-                         when "manifest"
-                           "QueryManifest"
-                         when "commands"
-                           "QueryCommands"
-                         when "types"
-                           "QueryTypes"
-                         when "entities"
-                           "QueryEntities"
-                         else
-                           raise "Not sure what to do with #{action}"
-                         end
-
-          full_command_name = "Foobara::CommandConnector::#{command_name}"
+          # :nocov:
+          raise "Not sure what to do with #{action}"
+          # :nocov:
         end
 
         registry_entry = command_registry[full_command_name]
 
         unless registry_entry
+          # :nocov:
           raise "Could not find command registered for #{path}"
+          # :nocov:
         end
 
         self.class::Request.new(registry_entry, **context)
