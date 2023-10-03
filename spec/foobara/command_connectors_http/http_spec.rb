2e4c38b0a8feb7e680fa17b4fc8c9ab57e3d6763
Miles Georgi <azimux@gmail.com>
2023-10-03 15:56:39 +0000

    99.27

--- spec/foobara/command_connectors_http/http_spec.rb
+++ spec/foobara/command_connectors_http/http_spec.rb
@@ -130,6 +130,19 @@ RSpec.describe Foobara::CommandConnectors::Http do
         expect(response.headers).to eq({})
         expect(response.body).to eq("16")
       end
+
+      context "with transformer instance instead of class" do
+        let(:inputs_transformers) { [inputs_transformer.instance] }
+
+        it "runs the command" do
+          expect(outcome).to be_success
+          expect(result).to be(16)
+
+          expect(response.status).to be(200)
+          expect(response.headers).to eq({})
+          expect(response.body).to eq("16")
+        end
+      end
     end
 
     context "with an entity input" do
