88a3fdf7e1bc27725c798adc72db1f53e06e15ce
Miles Georgi <azimux@gmail.com>
2023-10-04 21:39:39 +0000

    Stamp out element_type for associative array type

---
+++ projects/type_declarations/src/handlers/extend_associative_array_type_declaration/to_type_transformer.rb
@@ -0,0 +1,28 @@
+Foobara.require_file(
+  "type_declarations",
+  "handlers/extend_registered_type_declaration/to_type_transformer"
+)
+
+module Foobara
+  module TypeDeclarations
+    module Handlers
+      class ExtendAssociativeArrayTypeDeclaration < ExtendRegisteredTypeDeclaration
+        class ToTypeTransformer < ExtendRegisteredTypeDeclaration::ToTypeTransformer
+          def transform(strict_type_declaration)
+            super.tap do |type|
+              key_type_declaration = type.declaration_data[:key_type_declaration]
+              value_type_declaration = type.declaration_data[:value_type_declaration]
+
+              if key_type_declaration || value_type_declaration
+                type.element_type = [
+                  type_for_declaration(key_type_declaration || :duck),
+                  type_for_declaration(value_type_declaration || :duck)
+                ]
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
