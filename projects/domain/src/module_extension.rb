2ca041fc21abd9100308ce655f974dcbfcc2b708
Miles Georgi <azimux@gmail.com>
2023-12-07 00:19:07 +0000

    fix org

--- projects/domain/src/module_extension.rb
+++ projects/domain/src/module_extension.rb
@@ -37,8 +37,9 @@ module Foobara
         foobara_autoset_namespace!(default_namespace: Foobara)
         foobara_autoset_scoped_path!
 
-        # TODO: wow this is awkward. We should find a cleaner way to set childen on namespaces.
+        # TODO: wow this is awkward. We should find a cleaner way to set children on namespaces.
         parent = foobara_parent_namespace
+        parent.foobara_register(self)
         self.foobara_parent_namespace = parent
       end
 
