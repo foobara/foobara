7ae44a9fb29b5ee77b9302b86f12e737afe3eb54
Miles Georgi <azimux@gmail.com>
2023-12-24 18:40:11 +0000

    1703443211

---
+++ projects/type_declarations/src/handlers/registered_type_declaration/short_type_name_desugarizer.rb
@@ -0,0 +1,27 @@
+Foobara.require_file("type_declarations", "desugarizer")
+
+module Foobara
+  module TypeDeclarations
+    module Handlers
+      class RegisteredTypeDeclaration < TypeDeclarationHandler
+        class ShortTypeNameDesugarizer < TypeDeclarations::Desugarizer
+          def applicable?(sugary_type_declaration)
+            if sugary_type_declaration.is_a?(Hash) && sugary_type_declaration.key?(:type)
+              type_registered?(sugary_type_declaration[:type])
+            end
+          end
+
+          def desugarize(sugary_type_declaration)
+            type = type_for_symbol(sugary_type_declaration[:type])
+
+            # TODO: just use the symbol and nothing else??
+            # maybe confusing in languages with no distinction between symbol and string?
+            sugary_type_declaration[:type] = type.foobara_manifest_reference.to_sym
+
+            sugary_type_declaration
+          end
+        end
+      end
+    end
+  end
+end
