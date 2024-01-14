ef857711165866a4633cbff5d9d27dea3edde348
Miles Georgi <azimux@gmail.com>
2024-01-14 13:21:58 -0800

    1705267318

--- extract_repo.rb
+++ extract_repo.rb
@@ -3,7 +3,12 @@
 require "English"
 
 class ExtractRepo
-  attr_reader :repo_url, :paths
+  class << self
+    def run!(repo_url, paths)
+      new(repo_url, paths).execute
+    end
+  end
+  attr_reader :repo_url, :paths, :file_paths
 
   def initialize(repo_url, paths)
     self.repo_url = repo_url
@@ -15,11 +20,12 @@ class ExtractRepo
     rm_old_repo
 
     clone_repo
+    remove_origin
+    remove_tags
     determine_paths
-  end
-
-  def chdir(&)
-    Dir.chdir(extract_dir, &)
+    determine_historic_paths
+    filter_repo
+    remove_replaces
   end
 
   def extract_dir
@@ -48,14 +54,70 @@ class ExtractRepo
     end
   end
 
+  def remove_origin
+    Dir.chdir repo_dir do
+      sh "git remote rm origin"
+    end
+  end
+
+  def remove_tags
+    Dir.chdir repo_dir do
+      sh "for i in `git tag`; do git tag -d $i; done"
+    end
+  end
+
+  def remove_replaces
+    Dir.chdir repo_dir do
+      sh "git replace -l | xargs -n 1 git replace -d"
+    end
+  end
+
   def determine_paths
     Dir.chdir repo_dir do
-      paths.each do |path|
+      self.file_paths = []
+
+      Dir.chdir repo_dir do
+        paths.each do |path|
+          unless File.exist?(path)
+            raise "Path #{path} does not exist in repo #{repo_dir}"
+          end
+
+          if File.directory?(path)
+            Dir.glob("#{path}/**/*").each do |file|
+              file_paths << file if File.file?(file)
+            end
+          else
+            file_paths << path
+          end
+        end
       end
     end
   end
 
-  def sh(cmd)
+  def determine_historic_paths
+    Dir.chdir repo_dir do
+      file_paths.dup.each do |file_path|
+        historic_paths = sh "git log --follow --name-only --pretty=format: -- \"#{file_path}\""
+
+        historic_paths.split("\n").each do |historic_path|
+          file_paths << historic_path
+        end
+      end
+    end
+  end
+
+  def filter_repo
+    Dir.chdir repo_dir do
+      path_args = file_paths.map { |path| "--path #{path}" }.join(" ")
+      sh "git-filter-repo #{path_args} --force --prune-degenerate always", dry_run: true
+    end
+  end
+
+  def sh(cmd, dry_run: false)
+    puts cmd
+
+    return if dry_run
+
     result = `#{cmd}`
 
     unless $CHILD_STATUS.success?
@@ -63,6 +125,9 @@ class ExtractRepo
     end
   end
 end
+
+repo_url, *paths = ARGV
+ExtractRepo.run!(repo_url, paths)
 
 =begin
 a_to_s() {
