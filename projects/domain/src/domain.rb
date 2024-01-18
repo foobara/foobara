3548f035afdab96701dbc76c1e9c33bb956e3e65
Miles Georgi <azimux@gmail.com>
2024-01-18 01:13:36 +0000

    Add ./#foobara_type_from_declaration

--- projects/domain/src/domain.rb
+++ projects/domain/src/domain.rb
@@ -15,6 +15,12 @@ module Foobara
           Util.make_module(full_domain_name) { foobara_domain! }
         end
       end
+
+      def foobara_type_from_declaration(scoped, type_declaration)
+        domain = to_domain(scoped)
+
+        domain.foobara_type_from_declaration(type_declaration)
+      end
     end
   end
 end
