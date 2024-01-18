50794857fb330f8c73f8799aa4347ae58d1c8b4d
Miles Georgi <azimux@gmail.com>
2024-01-18 01:15:18 +0000

    Add Namespace.current/global/use

--- projects/namespace/src/namespace.rb
+++ projects/namespace/src/namespace.rb
@@ -2,6 +2,38 @@ module Foobara
   class Namespace
     include IsNamespace
 
+    class << self
+      attr_accessor :global
+
+      def current
+        Thread.current[:foobara_current_namespace] || global
+      end
+
+      # TODO: eliminate deprecated_namespace and yield instead
+      def use(namespace, deprecated_namespace, &)
+        unless namespace.is_a?(Namespace::IsNamespace)
+          # :nocov:
+          raise ArgumentError, "Expected #{namespace} to be a namespace"
+          # :nocov:
+        end
+
+        old_namespace = current
+
+        if old_namespace == namespace
+          TypeDeclarations::Namespace.using(deprecated_namespace, &)
+        else
+          begin
+            Thread.current[:foobara_current_namespace] = namespace
+            TypeDeclarations::Namespace.using(deprecated_namespace, &)
+          ensure
+            Thread.current[:foobara_current_namespace] = old_namespace
+          end
+        end
+      end
+    end
+
+    self.global = Foobara
+
     class NotFoundError < StandardError; end
 
     def initialize(scoped_name_or_path = nil, parent_namespace: nil)
