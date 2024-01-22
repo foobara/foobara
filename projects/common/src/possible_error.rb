4a0d534165f104d174007b6776f4582b3a91715b
Miles Georgi <azimux@gmail.com>
2024-01-22 09:45:30 -0800

    Add PossibleError

---
+++ projects/common/src/possible_error.rb
@@ -0,0 +1,27 @@
+module Foobara
+  class PossibleError
+    attr_accessor :key, :error_class, :data
+
+    def initialize(error_class, key: nil, data: nil, symbol: error_class.symbol, category: error_class.category)
+      self.error_class = error_class
+      self.data = data
+      self.key = if key
+                   if key.is_a?(ErrorKey)
+                     key
+                   else
+                     ErrorKey.parse(key)
+                   end
+                 else
+                   ErrorKey.new(symbol:, category:)
+                 end
+    end
+
+    def prepend_path!(...)
+      key.prepend_path!(...)
+    end
+
+    def prepend_runtime_path!(...)
+      key.prepend_runtime_path!(...)
+    end
+  end
+end
---
+++ spec/foobara/common/possible_error_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe Foobara::PossibleError do
+  describe "#initialize" do
+    context "with key and error_class" do
+      let(:possible_error) { described_class.new(error_class, key:) }
+
+      let(:key) { "data.foo.bar.baz" }
+      let(:error_class) { Foobara::RuntimeError }
+
+      it "creates an instance" do
+        expect(possible_error.key.path).to eq(%i[foo bar])
+        expect(possible_error.key.symbol).to eq(:baz)
+        expect(possible_error.key.category).to eq(:data)
+        expect(possible_error.error_class).to eq(Foobara::RuntimeError)
+      end
+    end
+  end
+end
