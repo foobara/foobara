6f1d614e7b47008d046dade044a036824b38e172
Miles Georgi <azimux@gmail.com>
2024-01-24 00:20:38 +0000

    Add #foobara_manifest to PossibleError

--- projects/common/src/possible_error.rb
+++ projects/common/src/possible_error.rb
@@ -43,5 +43,32 @@ module Foobara
     def prepend_runtime_path!(...)
       key.prepend_runtime_path!(...)
     end
+
+    # TODO: technically does not belong in this project but maybe it should
+    def foobara_manifest(to_include:)
+      to_include << error_class
+
+      if processor
+        processor_class = processor.class
+        to_include << processor_class
+
+        if processor.scoped_path_set?
+          # Unclear why nothing in the test suite passes through here.
+          # TODO: either test this or delete it.
+          # :nocov:
+          to_include << processor
+          processor_reference = processor.foobara_manifest_reference
+          # :nocov:
+        end
+      end
+
+      key.to_h.merge(
+        key: key.to_s,
+        error: error_class.foobara_manifest_reference,
+        processor: processor_reference,
+        processor_class: processor_class&.foobara_manifest_reference,
+        data:
+      )
+    end
   end
 end
