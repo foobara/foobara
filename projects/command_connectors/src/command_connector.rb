e4bae55b0717a7ca454261ec0838bc333d1f4abc
Miles Georgi <azimux@gmail.com>
2024-01-30 00:15:24 +0000

    Move most of request_to_command to base class.

--- projects/command_connectors/src/command_connector.rb
+++ projects/command_connectors/src/command_connector.rb
@@ -73,10 +73,90 @@ module Foobara
 
     attr_accessor :command_registry, :authenticator
 
-    def request_to_command(_request)
-      # :nocov:
-      raise "subclass responsibility"
-      # :nocov:
+    def request_to_command(request)
+      action = request.action
+      inputs = nil
+      full_command_name = request.full_command_name
+
+      case action
+      when "run"
+        transformed_command_class = command_registry[full_command_name]
+
+        unless transformed_command_class
+          # :nocov:
+          raise NoCommandFoundError, "Could not find command registered for #{full_command_name}"
+          # :nocov:
+        end
+
+        request.command_class = transformed_command_class
+
+        inputs = request.inputs
+      when "describe"
+        manifestable = transformed_command_from_name(full_command_name) || type_from_name(full_command_name)
+
+        unless manifestable
+          # :nocov:
+          raise NoCommandOrTypeFoundError, "Could not find command or type registered for #{full_command_name}"
+          # :nocov:
+        end
+
+        command_class = Foobara::CommandConnectors::Commands::Describe
+        full_command_name = command_class.full_command_name
+
+        inputs = { manifestable: }
+        transformed_command_class = command_registry[full_command_name] || transform_command_class(command_class)
+      when "describe_command"
+        transformed_command_class = transformed_command_from_name(full_command_name)
+
+        unless transformed_command_class
+          # :nocov:
+          raise NoCommandFoundError, "Could not find command registered for #{full_command_name}"
+          # :nocov:
+        end
+
+        command_class = Foobara::CommandConnectors::Commands::Describe
+        full_command_name = command_class.full_command_name
+
+        inputs = { manifestable: transformed_command_class }
+        transformed_command_class = command_registry[full_command_name] || transform_command_class(command_class)
+      when "describe_type"
+        type = type_from_name(full_command_name)
+
+        unless type
+          # :nocov:
+          raise NoTypeFoundError, "Could not find type registered for #{full_command_name}"
+          # :nocov:
+        end
+
+        command_class = Foobara::CommandConnectors::Commands::Describe
+        full_command_name = command_class.full_command_name
+
+        inputs = { manifestable: type }
+        transformed_command_class = command_registry[full_command_name] || transform_command_class(command_class)
+      when "manifest"
+        command_class = Foobara::CommandConnectors::Commands::Describe
+        full_command_name = command_class.full_command_name
+
+        inputs = { manifestable: self }
+        transformed_command_class = command_registry[full_command_name] || transform_command_class(command_class)
+      when "ping"
+        command_class = Foobara::CommandConnectors::Commands::Ping
+        full_command_name = command_class.full_command_name
+
+        transformed_command_class = command_registry[full_command_name] || transform_command_class(command_class)
+      when "query_git_commit_info"
+        # TODO: this feels out of control... should just accomplish this through run I think instead. Same with ping.
+        command_class = Foobara::CommandConnectors::Commands::QueryGitCommitInfo
+        full_command_name = command_class.full_command_name
+
+        transformed_command_class = command_registry[full_command_name] || transform_command_class(command_class)
+      else
+        # :nocov:
+        raise InvalidContextError, "Not sure what to do with #{action}"
+        # :nocov:
+      end
+
+      transformed_command_class.new(inputs)
     end
 
     def command_to_request(_command)
