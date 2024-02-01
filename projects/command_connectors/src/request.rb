b7aca392c3a546ae4a03cd769586a26f6596344e
Miles Georgi <azimux@gmail.com>
2024-02-01 01:21:35 +0000

    Add serializer and response body support to Request

--- projects/command_connectors/src/request.rb
+++ projects/command_connectors/src/request.rb
@@ -2,7 +2,7 @@ module Foobara
   module CommandConnectors
     class Request
       # TODO: this feels like a smell of some sort...
-      attr_accessor :command_class, :command, :error, :command_connector
+      attr_accessor :command_class, :command, :error, :command_connector, :serializers
 
       def full_command_name
         # :nocov:
@@ -14,6 +14,83 @@ module Foobara
         # :nocov:
         raise "subclass responsibility"
         # :nocov:
+      end
+
+      def serializer
+        return @serializer if defined?(@serializer)
+
+        if serializers.nil? || serializers.empty?
+          @serializer = nil
+          return
+        end
+
+        actual_serializers = objects_to_serializers(serializers)
+
+        @serializer = if actual_serializers.size == 1
+                        actual_serializers.first
+                      else
+                        Value::Processor::Pipeline.new(processors: actual_serializers)
+                      end
+      end
+
+      def response_body
+        @response_body ||= begin
+          # TODO: should we have separate ways to register success and failure serializers?
+          body = success? ? result : error_collection
+
+          if serializer
+            serializer.process_value!(body)
+          else
+            body
+          end
+        end
+      end
+
+      def success?
+        outcome.success?
+      end
+
+      def outcome
+        command.outcome
+      end
+
+      def result
+        outcome.result
+      end
+
+      def error_collection
+        outcome.error_collection
+      end
+
+      private
+
+      def objects_to_serializers(objects)
+        objects.map do |object|
+          case object
+          when Class
+            object.new(self)
+          when Value::Processor
+            object
+          when ::Symbol, ::String
+            klass = Serializer.serializer_from_symbol(object)
+
+            unless klass
+              # :nocov:
+              raise "Could not find serializer for #{object}"
+              # :nocov:
+            end
+
+            klass.new(self)
+          else
+            if object.respond_to?(:call)
+              Value::Transformer.create(transform: object)
+            else
+              # :nocov:
+              raise "Not sure how to convert #{object} into a serializer"
+              # :nocov:
+            end
+          end
+        end
       end
     end
   end
---
+++ spec/foobara/command_connectors/request_spec.rb
@@ -0,0 +1,95 @@
+RSpec.describe Foobara::CommandConnectors::Request do
+  let(:request) do
+    described_class.new
+  end
+
+  describe "#serializer" do
+    before do
+      request.serializers = serializers
+    end
+
+    context "when there's no serializers" do
+      let(:serializers) { nil }
+
+      it "is nil" do
+        expect(request.serializer).to be_nil
+      end
+    end
+
+    context "when there is a serializer" do
+      let(:serializers) { [:yaml] }
+
+      it "returns the serializer" do
+        expect(request.serializer.process_value!(foo: 1)).to eq("---\n:foo: 1\n")
+      end
+
+      context "when serializer is declared as a class" do
+        let(:serializers) { [Foobara::CommandConnectors::Serializers::YamlSerializer] }
+
+        it "returns the serializer" do
+          expect(request.serializer.process_value!(foo: 1)).to eq("---\n:foo: 1\n")
+        end
+      end
+
+      context "when serializer is an instance" do
+        let(:serializers) { [Foobara::CommandConnectors::Serializers::YamlSerializer.new("some request")] }
+
+        it "returns the serializer" do
+          expect(request.serializer.process_value!(foo: 1)).to eq("---\n:foo: 1\n")
+        end
+      end
+
+      context "when serializer is a proc" do
+        let(:serializers) { [proc { "100" }] }
+
+        it "returns the serializer" do
+          expect(request.serializer.process_value!(foo: 1)).to eq("100")
+        end
+      end
+    end
+
+    context "when there are multiple serializers" do
+      let(:serializers) { %i[yaml json] }
+
+      it "returns the serializer" do
+        expect(request.serializer.processors.size).to eq(2)
+      end
+    end
+  end
+
+  context "with a ran command" do
+    let(:command_class) do
+      stub_class("SomeCommand", Foobara::Command) do
+        inputs a: :integer
+
+        def execute
+          a * a
+        end
+      end
+    end
+
+    let(:command) { command_class.new(inputs).tap(&:run) }
+    let(:inputs) { { a: 4 } }
+    let(:serializers) { [] }
+
+    before do
+      request.command = command
+      request.serializers = serializers
+    end
+
+    describe "#response_body" do
+      it "gives the response body" do
+        expect(request.error_collection).to be_empty
+        expect(request.response_body).to eq(16)
+      end
+
+      context "with a serializer" do
+        let(:serializers) { [:json] }
+
+        it "gives the response body" do
+          expect(request.response_body).to eq("16")
+        end
+      end
+    end
+  end
+end
