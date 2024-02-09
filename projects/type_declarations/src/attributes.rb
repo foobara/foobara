6995b555c2952d8153771902eb7e705d2cb42402
Miles Georgi <azimux@gmail.com>
2024-02-08 21:10:20 -0800

    Refactor to an Attributes.merge method

---
+++ projects/type_declarations/src/attributes.rb
@@ -0,0 +1,34 @@
+module Foobara
+  module TypeDeclarations
+    module Attributes
+      class << self
+        def merge(*type_declarations)
+          element_type_declarations = {}
+          required = []
+          defaults = {}
+
+          type_declarations.each do |declaration_data|
+            element_type_declarations.merge!(declaration_data[:element_type_declarations])
+            type_defaults = declaration_data[:defaults]
+            type_required = declaration_data[:required]
+
+            if type_defaults && !type_defaults.empty?
+              defaults.merge!(type_defaults)
+            end
+
+            if type_required && !type_required.empty?
+              required += type_required
+            end
+          end
+
+          {
+            type: :attributes,
+            element_type_declarations:,
+            required:,
+            defaults:
+          }
+        end
+      end
+    end
+  end
+end
