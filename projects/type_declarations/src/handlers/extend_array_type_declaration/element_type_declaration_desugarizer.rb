a8781ba0f004acc3315dbeaf4a42220bacbc3d54
Miles Georgi <azimux@gmail.com>
2024-02-10 00:06:13 +0000

    Add ElementTypeDeclarationDesugarizer for Array type

---
+++ projects/type_declarations/src/handlers/extend_array_type_declaration/element_type_declaration_desugarizer.rb
@@ -0,0 +1,37 @@
+module Foobara
+  module TypeDeclarations
+    module Handlers
+      class ExtendArrayTypeDeclaration < ExtendAssociativeArrayTypeDeclaration
+        class ElementTypeDeclarationDesugarizer < TypeDeclarations::Desugarizer
+          def applicable?(sugary_type_declaration)
+            return false unless sugary_type_declaration.is_a?(::Hash)
+            return false unless Util.all_symbolizable_keys?(sugary_type_declaration)
+
+            sugary_type_declaration = Util.symbolize_keys(sugary_type_declaration)
+
+            return false unless sugary_type_declaration.key?(:type)
+
+            type_symbol = sugary_type_declaration[:type]
+
+            type_symbol == :array && sugary_type_declaration.key?(:element_type_declaration)
+          end
+
+          def desugarize(sugary_type_declaration)
+            sugar = sugary_type_declaration[:element_type_declaration]
+
+            handler = type_declaration_handler_for(sugar)
+            strict = handler.desugarize(sugar)
+
+            sugary_type_declaration[:element_type_declaration] = strict
+
+            sugary_type_declaration
+          end
+
+          def priority
+            Priority::LOW
+          end
+        end
+      end
+    end
+  end
+end
