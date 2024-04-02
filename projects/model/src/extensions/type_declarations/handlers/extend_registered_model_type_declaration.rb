899d9854013e204ffc02bc8ad8092ccf9622e9a3
Miles Georgi <azimux@gmail.com>
2024-04-02 14:10:07 -0700

    1712092207

--- projects/model/src/extensions/type_declarations/handlers/extend_registered_model_type_declaration.rb
+++ projects/model/src/extensions/type_declarations/handlers/extend_registered_model_type_declaration.rb
@@ -10,15 +10,6 @@ module Foobara
 
             return false if type_symbol == expected_type_symbol
 
-            # If there's only one element, then we are probably attempting a registered type lookup, not
-            # extending a registered model to create a new type.
-            case strict_type_declaration.size
-            when 1
-              return false
-            when 2
-              return false if strict_type_declaration.key?(:_desugarized)
-            end
-
             if type_registered?(type_symbol)
               type = type_for_declaration(type_symbol)
               type.extends?(BuiltinTypes[expected_type_symbol])
