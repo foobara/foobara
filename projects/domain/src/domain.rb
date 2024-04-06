b84512285beb0660292abae19a1a915ff33d7e5b
Miles Georgi <azimux@gmail.com>
2024-04-06 13:42:11 -0700

    Move .to_org/domain to domain.rb

--- projects/domain/src/domain.rb
+++ projects/domain/src/domain.rb
@@ -4,6 +4,74 @@ module Foobara
     class OrganizationAlreadyExistsError < StandardError; end
 
     class << self
+      # TODO: move this to organization.rb
+      def to_organization(object)
+        case object
+        when nil
+          GlobalOrganization
+        when ::String, ::Symbol
+          organization = Namespace.global.foobara_lookup_organization(object)
+
+          unless organization
+            # :nocov:
+            raise NoSuchOrganization, "Couldn't determine organization for #{object}"
+            # :nocov:
+          end
+
+          organization
+        when Foobara::Scoped
+          if object.is_a?(Module) && object.foobara_organization?
+            object
+          else
+            parent = object.scoped_namespace
+
+            if parent
+              to_organization(parent)
+            else
+              GlobalOrganization
+            end
+          end
+        else
+          # :nocov:
+          raise NoSuchOrganization, "Couldn't determine organization for #{object}"
+          # :nocov:
+        end
+      end
+
+      # TODO: move this to domain.rb
+      def to_domain(object)
+        case object
+        when nil
+          global
+        when ::String, ::Symbol
+          domain = Namespace.global.foobara_lookup_domain(object)
+
+          unless domain
+            # :nocov:
+            raise NoSuchDomain, "Couldn't determine domain for #{object}"
+            # :nocov:
+          end
+
+          domain
+        when Foobara::Scoped
+          if object.is_a?(Module) && object.foobara_domain?
+            object
+          else
+            parent = object.scoped_namespace
+
+            if parent
+              to_domain(parent)
+            else
+              GlobalDomain
+            end
+          end
+        else
+          # :nocov:
+          raise NoSuchDomain, "Couldn't determine domain for #{object}"
+          # :nocov:
+        end
+      end
+
       def create(full_domain_name)
         if Domain.to_domain(full_domain_name)
           raise DomainAlreadyExistsError, "Domain #{full_domain_name} already exists"
