dc521b0e0df09a2e000b0062f2586e8040641ee4
Miles Georgi <azimux@gmail.com>
2024-04-12 21:09:55 -0500

    Allow specifying any class as a type and have it be a :duck that is instance_of that class

---
+++ projects/builtin_types/src/duck/supported_validators/instance_of.rb
@@ -0,0 +1,42 @@
+module Foobara
+  module BuiltinTypes
+    module Duck
+      module SupportedValidators
+        class InstanceOf < TypeDeclarations::Validator
+          class NotInstanceOfError < Foobara::Value::DataError
+            class << self
+              def context_type_declaration
+                {
+                  value: :duck,
+                  expected_class_name: :string
+                }
+              end
+            end
+          end
+
+          def expected_class_name
+            declaration_data
+          end
+
+          def validation_errors(value)
+            klass = Object.const_get(expected_class_name)
+            unless value.is_a?(klass)
+              build_error(value)
+            end
+          end
+
+          def error_message(value)
+            "#{value} is not an instance of #{expected_class_name}"
+          end
+
+          def error_context(value)
+            {
+              value:,
+              expected_class_name:
+            }
+          end
+        end
+      end
+    end
+  end
+end
