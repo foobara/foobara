da7db5a05c4e0e826297b47baa9e25fed050038f
Miles Georgi <azimux@gmail.com>
2024-04-26 21:23:12 -0700

    1714191792

---
+++ spec/foobara/command/concerns/domain_mapper_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe Foobara::Command::Concerns::DomainMappers do
+  describe "#run_mapped_subcommand!" do
+    let(:domain) do
+      stub_module("SomeDomain") do
+        foobara_domain!
+      end
+    end
+    let(:command_class) do
+      subcommand_class
+      stub_class("SomeDomain::SomeCommand", Foobara::Command) do
+        depends_on SomeDomain::SomeSubcommand
+
+        def execute
+          run_mapped_subcommand!(SomeDomain::SomeSubcommand, "bar")
+        end
+      end
+    end
+
+    let(:subcommand_class) do
+      domain
+      stub_class("SomeDomain::SomeSubcommand", Foobara::Command) do
+        inputs foo: :string
+
+        def execute
+          inputs
+        end
+      end
+    end
+
+    let(:domain_mapper) do
+      subcommand_class
+      stub_module("SomeDomain::DomainMappers")
+      stub_class("SomeDomain::DomainMappers::SomeDomainMapper", Foobara::DomainMapper) do
+        from :string
+        to SomeDomain::SomeSubcommand
+
+        # TODO: pass from_value in instead for improved readability
+        def call
+          { foo: from_value }
+        end
+      end
+    end
+
+    it "maps the inputs", :focus do
+      expect(outcome).to be_success
+
+      expect(result).to eq(foo: "bar")
+    end
+  end
+end
