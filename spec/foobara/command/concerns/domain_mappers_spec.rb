e047d14a3eb08263d8afa8f2298049a2e62e1882
Miles Georgi <azimux@gmail.com>
2024-04-28 17:50:36 -0700

    1714351836

--- spec/foobara/command/concerns/domain_mappers_spec.rb
+++ spec/foobara/command/concerns/domain_mappers_spec.rb
@@ -15,7 +15,12 @@ RSpec.describe Foobara::Command::Concerns::DomainMappers do
         depends_on SomeDomain::SomeSubcommand
 
         def execute
-          run_mapped_subcommand!(SomeDomain::SomeSubcommand, "bar")
+          subcommand_inputs1 = domain_map("bar", to: SomeDomain::SomeSubcommand, from: :string, strict: true)
+          subcommand_inputs2 = run_mapped_subcommand!(SomeDomain::SomeSubcommand, "bar")
+
+          raise "wtf" unless subcommand_inputs1 == subcommand_inputs2
+
+          subcommand_inputs1
         end
       end
     end
@@ -45,11 +50,25 @@ RSpec.describe Foobara::Command::Concerns::DomainMappers do
       end
     end
 
+    let(:result_domain_mapper) do
+      subcommand_class
+      stub_module("SomeDomain::DomainMappers")
+      stub_class("SomeDomain::DomainMappers::SomeResultDomainMapper", Foobara::DomainMapper) do
+        from SomeDomain::DomainMappers::SomeDomainMapper.to_type
+        to :string
+
+        # TODO: pass from_value in instead for improved readability
+        def map(from_value)
+          JSON.generate(from_value)
+        end
+      end
+    end
+
     let(:command) { command_class.new }
     let(:outcome) { command.run }
     let(:result) { outcome.result }
 
-    it "maps the inputs", :focus  do
+    it "maps the inputs" do
       domain_mapper
       expect(outcome).to be_success
       expect(result).to eq(foo: "bar")
