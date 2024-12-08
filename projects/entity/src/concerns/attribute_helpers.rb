2e8faa8efd4eeddf028ce5af1d14ca3419a58ef1
Miles Georgi <azimux@gmail.com>
2024-12-08 13:00:07 -0800

    Move #/.update_aggregate from entity AttributeHelpers to Mutations

--- projects/entity/src/concerns/attribute_helpers.rb
+++ projects/entity/src/concerns/attribute_helpers.rb
@@ -4,11 +4,6 @@ module Foobara
       module AttributeHelpers
         include Foobara::Concern
 
-        def update_aggregate(value, type = self.class.model_type)
-          # is this a smell?
-          self.class.update_aggregate(self, value, type)
-        end
-
         module ClassMethods
           def attributes_for_update
             attributes_for_aggregate_update
@@ -109,58 +104,6 @@ module Foobara
               type: "::attributes",
               element_type_declarations:
             )
-          end
-
-          def update_aggregate(object, value, type = object.class.model_type)
-            return value if object.nil?
-
-            if type.extends?(BuiltinTypes[:model])
-              element_types = type.element_types.element_types
-
-              value.each_pair do |attribute_name, new_value|
-                current_value = object.read_attribute(attribute_name)
-
-                attribute_type = element_types[attribute_name]
-
-                updated_value = update_aggregate(current_value, new_value, attribute_type)
-
-                object.write_attribute(attribute_name, updated_value)
-              end
-
-              object
-            elsif type.extends?(BuiltinTypes[:attributes])
-              element_types = type.element_types
-
-              object = object.dup
-              object ||= {}
-
-              value.each_pair do |attribute_name, new_value|
-                current_value = object[attribute_name]
-                attribute_type = element_types[attribute_name]
-
-                updated_value = update_aggregate(current_value, new_value, attribute_type)
-
-                object[attribute_name] = updated_value
-              end
-
-              object
-            elsif type.extends?(BuiltinTypes[:tuple])
-              # :nocov:
-              raise "Tuple not yet supported"
-            # :nocov:
-            elsif type.extends?(BuiltinTypes[:associative_array])
-              # :nocov:
-              raise "Associated array not yet supported"
-            # :nocov:
-            elsif type.extends?(BuiltinTypes[:array])
-              element_type = type.element_type
-
-              value.map.with_index do |element, index|
-                update_aggregate(object[index], element, element_type)
-              end
-            else
-              value
-            end
           end
 
           private
--- projects/entity/src/concerns/mutations.rb
+++ projects/entity/src/concerns/mutations.rb
@@ -4,7 +4,63 @@ module Foobara
       module Mutations
         include Concern
 
+        def update_aggregate(value, type = self.class.model_type)
+          # is this a smell?
+          self.class.update_aggregate(self, value, type)
+        end
+
         module ClassMethods
+          def update_aggregate(object, value, type = object.class.model_type)
+            return value if object.nil?
+
+            if type.extends?(BuiltinTypes[:model])
+              element_types = type.element_types.element_types
+
+              value.each_pair do |attribute_name, new_value|
+                current_value = object.read_attribute(attribute_name)
+
+                attribute_type = element_types[attribute_name]
+
+                updated_value = update_aggregate(current_value, new_value, attribute_type)
+
+                object.write_attribute(attribute_name, updated_value)
+              end
+
+              object
+            elsif type.extends?(BuiltinTypes[:attributes])
+              element_types = type.element_types
+
+              object = object.dup
+              object ||= {}
+
+              value.each_pair do |attribute_name, new_value|
+                current_value = object[attribute_name]
+                attribute_type = element_types[attribute_name]
+
+                updated_value = update_aggregate(current_value, new_value, attribute_type)
+
+                object[attribute_name] = updated_value
+              end
+
+              object
+            elsif type.extends?(BuiltinTypes[:tuple])
+              # :nocov:
+              raise "Tuple not yet supported"
+              # :nocov:
+            elsif type.extends?(BuiltinTypes[:associative_array])
+              # :nocov:
+              raise "Associated array not yet supported"
+              # :nocov:
+            elsif type.extends?(BuiltinTypes[:array])
+              element_type = type.element_type
+
+              value.map.with_index do |element, index|
+                update_aggregate(object[index], element, element_type)
+              end
+            else
+              value
+            end
+          end
         end
       end
     end
