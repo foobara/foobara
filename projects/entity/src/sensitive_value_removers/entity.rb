a5f3223d54b778040aac1ad994891930553080b3
Miles Georgi <azimux@gmail.com>
2025-03-26 07:57:40 -0700

    Fix bug where removing values from sensitive records mishandles thunks

--- projects/entity/src/sensitive_value_removers/entity.rb
+++ projects/entity/src/sensitive_value_removers/entity.rb
@@ -3,12 +3,28 @@ module Foobara
     module SensitiveValueRemovers
       class Entity < DetachedEntity::SensitiveValueRemovers::DetachedEntity
         def transform(record)
-          sanitized_record = super
+          if record.loaded? || record.created?
+            sanitized_record = super
 
-          sanitized_record.is_loaded = record.loaded?
-          sanitized_record.is_persisted = record.persisted?
+            sanitized_record.is_loaded = record.loaded?
+            sanitized_record.is_persisted = record.persisted?
 
-          sanitized_record
+            sanitized_record
+          elsif record.persisted?
+            # We will assume that we do not need to clean up the primary key itself as
+            # we will assume we don't allow sensitive primary keys for now.
+            sanitized_record = to_type.target_class.build(record.class.primary_key_attribute => record.primary_key)
+
+            sanitized_record.is_persisted = true
+            sanitized_record.is_loaded = false
+            sanitized_record.is_built = false
+
+            sanitized_record
+          else
+            # :nocov:
+            raise "Not sure what to do with a record that isn't loaded, created, or persisted"
+            # :nocov:
+          end
         end
 
         def build_method
