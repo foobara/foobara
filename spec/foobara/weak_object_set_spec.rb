0dd8cb6e766aa3ac14caef052457bceaeca51af1
Miles Georgi <azimux@gmail.com>
2025-04-24 21:09:15 -0700

    Get WeakObjectSet spec working with GC.start

--- spec/foobara/weak_object_set_spec.rb
+++ spec/foobara/weak_object_set_spec.rb
@@ -1,42 +1,51 @@
 RSpec.describe Foobara::WeakObjectSet do
   let(:set) { described_class.new(key_method) }
-  let(:some_object) { "asdf" }
-  let(:some_object_id) { some_object.object_id }
-
-  before do
-    set << some_object
-  end
 
   describe "auto-removal of garbage collected records" do
     let(:key_method) { nil }
 
     context "without key_method" do
       it "automatically removes records that have been garbage collected" do
+        some_object = "asdf"
+        some_object_id = some_object.object_id
+        set << some_object
+
         expect(set.include?(some_object_id)).to be(true)
         expect(set.include?(some_object)).to be(true)
 
-        # NOTE: for whatever reason GC.start works for testing this in console but not in test suite.
-        # So directly triggering the cleanup proc instead.
-        set.garbage_cleaner.cleanup_proc.call(some_object_id)
+        # rubocop:disable Lint/UselessAssignment
+        some_object = nil
+        # rubocop:enable Lint/UselessAssignment
+        GC.start
 
         expect(set).to be_empty
       end
     end
 
     context "with key_method passed in" do
-      let(:key_method) { :length }
+      let(:some_class) do
+        stub_class "Foo" do
+          attr_accessor :bar
+        end
+      end
+      let(:key_method) { :bar }
 
       it "automatically removes records that have been garbage collected" do
+        some_object = some_class.new.tap { |o| o.bar = "asdf" }
+        some_object_id = some_object.object_id
+        set << some_object
+
         expect(set).to_not be_empty
         expect(set.size).to eq(1)
-        expect(set.include_key?(4)).to be(true)
+        expect(set.include_key?("asdf")).to be(true)
         expect(set.include?(some_object_id)).to be(true)
 
-        expect(set.to_a).to eq(["asdf"])
+        expect(set.to_a.map(&:object_id)).to eq([some_object_id])
 
-        # NOTE: for whatever reason GC.start works for testing this in console but not in test suite.
-        # So directly triggering the cleanup proc instead.
-        set.garbage_cleaner.cleanup_proc.call(some_object_id)
+        # rubocop:disable Lint/UselessAssignment
+        some_object = nil
+        # rubocop:enable Lint/UselessAssignment
+        GC.start
 
         expect(set).to be_empty
         expect(set.size).to eq(0)
@@ -44,16 +53,12 @@ RSpec.describe Foobara::WeakObjectSet do
       end
 
       describe "#<<" do
-        let(:some_class) do
-          stub_class "Foo" do
-            attr_accessor :bar
-          end
-        end
-        let(:some_object) { some_class.new.tap { |o| o.bar = "asdf" } }
-        let(:key_method) { :bar }
-
         context "when key is unset" do
           it "can no longer find the object by the old key" do
+            some_object = some_class.new.tap { |o| o.bar = "asdf" }
+            some_object_id = some_object.object_id
+            set << some_object
+
             expect(set.include_key?("asdf")).to be(true)
 
             some_object.bar = nil
