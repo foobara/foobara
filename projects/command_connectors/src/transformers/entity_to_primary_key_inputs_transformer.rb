76a3ff6c26d7f9afe0e3fa2ca1dd67faeaeff0ad
Miles Georgi <azimux@gmail.com>
2025-06-18 14:11:24 -0700

    Move EntityToPrimaryKeyInputsTransformer over from foobara-agent gem

---
+++ projects/command_connectors/src/transformers/entity_to_primary_key_inputs_transformer.rb
@@ -0,0 +1,75 @@
+module Foobara
+  module CommandConnectors
+    module Transformers
+      class EntityToPrimaryKeyInputsTransformer < TypeDeclarations::TypedTransformer
+        def from_type_declaration
+          return nil unless to_type
+
+          if contains_associations_or_is_entity?(to_type)
+            if to_type.extends?(Foobara::BuiltinTypes[:attributes])
+              to_fix = {}
+
+              to_type.element_types.each_pair do |attribute_name, attribute_type|
+                if contains_associations_or_is_entity?(attribute_type)
+                  to_fix[attribute_name] = attribute_type
+                end
+              end
+
+              element_type_declarations = to_type.declaration_data[:element_type_declarations].dup
+
+              to_fix.each_pair do |attribute_name, attribute_type|
+                transformer = EntityToPrimaryKeyInputsTransformer.new(to: attribute_type)
+                element_type_declarations[attribute_name] = transformer.from_type_declaration
+              end
+
+              to_type.declaration_data.merge(element_type_declarations:)
+            elsif to_type.extends?(Foobara::BuiltinTypes[:tuple])
+              indexes_to_fix = []
+
+              to_type.element_types.each.with_index do |element_type, index|
+                if contains_associations_or_is_entity?(element_type)
+                  indexes_to_fix << index
+                end
+              end
+
+              element_type_declarations = to_type.declaration_data[:element_type_declarations].dup
+
+              indexes_to_fix.each do |index|
+                transformer = EntityToPrimaryKeyInputsTransformer.new(to: to_type.element_types[index])
+                element_type_declarations[index] = transformer.from_type_declaration
+              end
+
+              to_type.declaration_data.merge(element_type_declarations:)
+            elsif to_type.extends?(Foobara::BuiltinTypes[:array])
+              transformer = EntityToPrimaryKeyInputsTransformer.new(to: to_type.element_type)
+              element_type_declaration = transformer.from_type_declaration
+
+              to_type.declaration_data.merge(element_type_declaration:)
+            elsif to_type.extends?(Foobara::BuiltinTypes[:detached_entity])
+              to_type.target_class.primary_key_type.reference_or_declaration_data
+            elsif to_type.extends?(Foobara::BuiltinTypes[:model])
+              attributes_type = to_type.target_class.attributes_type
+              EntityToPrimaryKeyInputsTransformer.new(to: attributes_type).from_type_declaration
+            else
+              # :nocov:
+              raise "Not sure how to handle #{to_type}"
+              # :nocov:
+            end
+          else
+            to_type
+          end
+        end
+
+        def transform(inputs)
+          inputs
+        end
+
+        private
+
+        def contains_associations_or_is_entity?(type)
+          DetachedEntity.contains_associations?(type) || type.extends?(Foobara::BuiltinTypes[:detached_entity])
+        end
+      end
+    end
+  end
+end
---
+++ spec/command_connectors/transformers/entity_to_primary_key_inputs_transformer_spec.rb
@@ -0,0 +1,101 @@
+RSpec.describe Foobara::CommandConnectors::Transformers::EntityToPrimaryKeyInputsTransformer do
+  after { Foobara.reset_alls }
+
+  before do
+    crud_driver = Foobara::Persistence::CrudDrivers::InMemory.new
+    Foobara::Persistence.default_crud_driver = crud_driver
+  end
+
+  let(:some_entity_class) do
+    stub_class "SomeEntity", Foobara::Entity do
+      attributes do
+        id :integer
+        foo :string
+        bar :string
+      end
+
+      primary_key :id
+    end
+  end
+  let(:some_model_class) do
+    some_entity_class
+
+    stub_class "SomeModel", Foobara::Model do
+      attributes do
+        some_entity SomeEntity
+        foo :string
+        bar :string
+      end
+    end
+  end
+
+  let(:type) do
+    some_model_class
+
+    Foobara::TypeDeclarations::Dsl::Attributes.to_declaration do
+      some_tuple [:integer, :string, SomeEntity]
+      some_array [SomeEntity]
+      some_model SomeModel
+    end
+  end
+
+  let(:value) do
+    some_record_id = some_record.id
+
+    some_entity_class.transaction do
+      {
+        some_tuple: [1, "foo", some_record_id],
+        some_array: [some_record_id],
+        some_model: { some_entity: some_record_id, foo: "foo", bar: "bar" }
+      }
+    end
+  end
+  let(:some_record) do
+    some_entity_class.transaction do
+      some_entity_class.create(foo: "foo", bar: "bar")
+    end
+  end
+
+  let(:transformer) { described_class.new(to: type) }
+
+  it "gives an expected from_type" do
+    expect(transformer.from_type.declaration_data).to eq(
+      type: :attributes,
+      element_type_declarations: {
+        some_tuple: {
+          type: :tuple,
+          element_type_declarations: [{ type: :integer }, { type: :string }, { type: :integer }],
+          size: 3
+        },
+        some_array: { type: :array, element_type_declaration: { type: :integer } },
+        some_model: {
+          type: :attributes,
+          element_type_declarations: {
+            some_entity: { type: :integer },
+            foo: { type: :string },
+            bar: { type: :string }
+          }
+        }
+
+      }
+    )
+  end
+
+  context "when type has no associations and isn't an entity" do
+    let(:type) { Foobara::BuiltinTypes[:string] }
+
+    it "has a from_type that is the to_type" do
+      expect(transformer.from_type).to eq(transformer.to_type)
+    end
+  end
+
+  describe "#transform" do
+    it "gives what was passed in because we expect this data to be cast where needed" do
+      expect(transformer.transform(value)).to eq(
+        some_tuple: [1, "foo", some_record.id],
+        some_array: [some_record.id],
+        some_model: { some_entity: some_record.id, foo: "foo", bar: "bar" }
+      )
+    end
+  end
+end
