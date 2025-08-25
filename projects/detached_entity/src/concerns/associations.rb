50cc806e21c251f995641f62cc46f228c84fbb32
Miles Georgi <azimux@gmail.com>
2025-08-25 09:59:42 -0700

    Fix incorrectly reported associations for entities detached locally

--- projects/detached_entity/src/concerns/associations.rb
+++ projects/detached_entity/src/concerns/associations.rb
@@ -163,7 +163,8 @@ module Foobara
 
             remove_sensitive = TypeDeclarations.foobara_manifest_context_remove_sensitive?
 
-            if type.extends?(BuiltinTypes[:entity])
+            if type.extends?(BuiltinTypes[:entity]) ||
+               (type.extends?(BuiltinTypes[:detached_entity]) && type.declaration_data[:detached_locally])
               result[path.to_s] = type
             elsif type.extends?(BuiltinTypes[:tuple])
               element_types = type.element_types
