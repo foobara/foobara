82a33c9a22c57a265058995766029e47cc3f3327
Miles Georgi <azimux@gmail.com>
2025-08-29 13:33:29 -0700

    Eliminate a use of foobara_delegate

--- projects/callback/src/block/concerns/type.rb
+++ projects/callback/src/block/concerns/type.rb
@@ -6,6 +6,8 @@ module Foobara
           include Concern
 
           module ClassMethods
+            # TODO: consider renaming this to symbol? Could be confused with Foobara::Type concept
+            # Returns things like :before, :after, :around, :error to indicate what type of callback it is
             def type
               @type ||= Util.non_full_name_underscore(self)&.gsub(/_block$/, "")&.to_sym
             end
