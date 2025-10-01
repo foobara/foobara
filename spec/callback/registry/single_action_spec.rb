5f83147c2de548417e4f1dfc0136280ba0988926
Miles Georgi <azimux@gmail.com>
2025-10-01 11:11:03 -0700

    Test SingleAction callback

---
+++ spec/callback/registry/single_action_spec.rb
@@ -0,0 +1,114 @@
+RSpec.describe Foobara::Callback::Registry::SingleAction do
+  let(:registry) { described_class.new }
+
+  describe "#register_callback" do
+    def register_it
+      registry.after { "noop" }
+    end
+
+    context "when given good action" do
+      it "registers it and can return it even when unioned with other actions" do
+        expect {
+          register_it
+        }.to change { registry.specific_callback_set_for.size }.from(0).to(1)
+
+        set = registry.unioned_callback_set_for
+        expect(set.size).to eq(1)
+      end
+    end
+  end
+
+  describe "#runner" do
+    context "when no action given" do
+      let(:runner) { registry.runner }
+
+      describe "#run" do
+        context "with block" do
+          it "calls expected callbacks in order" do
+            ran = false
+            callback_ran = false
+
+            registry.around do |&do_it|
+              do_it.call
+
+              callback_ran = true
+            end
+
+            expect {
+              runner.run { ran = true }
+            }.to change { ran }.from(false).to(true)
+
+            expect(callback_ran).to be(true)
+          end
+        end
+
+        context "with blocks" do
+          let(:callbacks_ran) { [] }
+
+          before do
+            expect(registry).to_not have_before_callbacks
+            expect(registry).to_not have_around_callbacks
+            expect(registry).to_not have_after_callbacks
+            expect(registry).to_not have_error_callbacks
+
+            registry.before do
+              callbacks_ran << :before
+            end
+
+            expect(registry).to have_before_callbacks
+            expect(registry).to_not have_around_callbacks
+            expect(registry).to_not have_after_callbacks
+            expect(registry).to_not have_error_callbacks
+
+            registry.around do |&do_it|
+              callbacks_ran << :around_start
+
+              do_it.call
+
+              callbacks_ran << :around_end
+            end
+
+            expect(registry).to have_before_callbacks
+            expect(registry).to have_around_callbacks
+            expect(registry).to_not have_after_callbacks
+            expect(registry).to_not have_error_callbacks
+
+            registry.after  do
+              callbacks_ran << :after
+            end
+
+            expect(registry).to have_before_callbacks
+            expect(registry).to have_around_callbacks
+            expect(registry).to have_after_callbacks
+            expect(registry).to_not have_error_callbacks
+
+            registry.error do |error|
+              expect(error).to be_a(Foobara::Callback::Runner::UnexpectedErrorWhileRunningCallback)
+              expect(error.message).to eq("kaboom!")
+
+              callbacks_ran << :error
+            end
+
+            expect(registry).to have_before_callbacks
+            expect(registry).to have_around_callbacks
+            expect(registry).to have_after_callbacks
+            expect(registry).to have_error_callbacks
+          end
+
+          it "calls expected callbacks in order" do
+            runner.run { "noop" }
+
+            expect(callbacks_ran).to eq([:before, :around_start, :around_end, :after])
+            callbacks_ran.clear
+
+            expect {
+              runner.run { raise "kaboom!" }
+            }.to raise_error(RuntimeError)
+
+            expect(callbacks_ran).to eq([:before, :around_start, :error])
+          end
+        end
+      end
+    end
+  end
+end
